8d8b27e450c837fb4f6abcdcb6097cf30a2f7a6f
Benjamin Carson <benjohncarson@gmail.com>
2017-03-06 11:45:03 +0000

    store test

--- spec/data_spec.rb
+++ spec/data_spec.rb
@@ -4,4 +4,12 @@ describe DataModel do
   it "initializes with an empty data_store hash" do
     expect(data_model).to have_attributes(:data_store => {})
   end
+  
+  describe "#store" do
+    it "adds data to data_store" do
+      data = { somekey: "somevalue" }
+      data_model.store(data)
+      expect(data_model.data_store).to include(data)
+    end
+  end
 end
